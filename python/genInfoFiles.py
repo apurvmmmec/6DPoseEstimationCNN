ffe4eab487e13cb4ba8d1071657b31aa430725e4
Apurv Nigam <apurv.mmmec@gmail.com>
2017-08-21 00:22:55 +0100

    Init commit of python code

---
+++ python/genInfoFiles.py
@@ -0,0 +1,72 @@
+import ruamel.yaml as yaml
+import numpy as np
+import inout as io
+import os
+
+
+from utils import *
+base_path = '/Users/apurvnigam/study_ucl/term1/MScThesis/hinterstoisser/'
+
+rgb_in_mpath = base_path+ 'test/{:02d}/rgb/{:04d}.png'
+model_mpath = base_path + 'models/obj_{:02d}.ply' # Already transformed
+seg_path = base_path+'test/{:02d}/seg/'
+model_info_path = base_path +'models/models_info.yml'
+info_file_path= base_path+'test/{:02d}/info/info_{:05d}.txt'
+img_w = 640
+img_h = 480
+
+for x in  [5]:
+
+    objId =scene_id =x;
+    scene_info_mpath = base_path + 'test/{:02d}/info.yml'
+    scene_gt_mpath = base_path + 'test/{:02d}/gt.yml'
+
+
+    with open(model_info_path,'r') as modelInfoFile:
+        modelInfo = yaml.load(modelInfoFile,Loader=yaml.CLoader)
+
+
+
+
+    with open(scene_gt_mpath.format(scene_id), 'r') as f:
+        gt_info = yaml.load(f, Loader=yaml.CLoader)
+
+
+    model = io.load_ply(model_mpath.format(objId))
+
+    numImages = len(gt_info)
+
+    for im_id in range(0,numImages):
+        print im_id
+        infoFile = open(info_file_path.format(objId,im_id), 'w')
+        [r,t]= getRotTrans(gt_info,im_id,objId)
+        # print 'image size'
+        # print img_w , img_h
+        # print 'ApeACCV'
+        #
+        # print 'rotatation:'
+        # print r[0,0], r[0,1], r[0,2]
+        # print r[1, 0], r[1, 1], r[1, 2]
+        # print r[2, 0], r[2, 1], r[2, 2]
+        # print 'centre:'
+        # print t[0, 0]/1000, t[1, 0]/1000, t[2, 0]/1000
+        # print 'extent:'
+        # print modelInfo[objId]['size_x'] / 1000, modelInfo[objId]['size_y'] / 1000, modelInfo[objId]['size_z'] / 1000
+
+        infoFile.writelines( 'image size\n')
+        infoFile.writelines('%d,%d\n'%( img_w, img_h))
+        infoFile.writelines( 'ApeACCV\n')
+
+        infoFile.writelines( 'rotation:\n')
+        infoFile.writelines( '%f %f %f\n'%(r[0, 0], r[0, 1], r[0, 2]))
+        infoFile.writelines( '%f %f %f\n'%(r[1, 0], r[1, 1], r[1, 2]))
+        infoFile.writelines( '%f %f %f\n'%(r[2, 0], r[2, 1], r[2, 2]))
+        infoFile.writelines( 'center:\n')
+
+
+
+        infoFile.writelines('%f %f %f\n'%( t[0, 0] / 1000, t[1, 0] / 1000, t[2, 0] / 1000))
+        infoFile.writelines( 'extent:\n')
+        infoFile.writelines( '%f %f %f\n'%(modelInfo[objId]['size_x'] / 1000, modelInfo[objId]['size_y'] / 1000, modelInfo[objId]['size_z'] / 1000))
+
+
--- python/separateTestTrain.py
+++ python/separateTestTrain.py
@@ -1,26 +1,69 @@
 import numpy as np
-
+import os
 
 base_path = '/Users/apurvnigam/study_ucl/term1/MScThesis/hinterstoisser/'
 trainScenes_file_path = base_path + 'test/{:02d}/trainingScenes.txt'
 testScenes_file_path = base_path + 'test/{:02d}/testScenes.txt'
 
+rgb_in_mpath = base_path + 'test/{:02d}/rgb/{:04d}.png'
+depth_in_mpath = base_path + 'test/{:02d}/depth/{:04d}.png'
+seg_in_path = base_path + 'test/{:02d}/seg/{:04d}.png'
+labelled_mpath = base_path + 'test/{:02d}/125bins/labelled/{:04d}.png'
+
+labelled_output_mpath = base_path+'test/{:02d}/testResults_125bins/labelled/{:04d}.png'
+seg_in_path = base_path + 'test/{:02d}/seg/{:04d}.png'
+info_in_path = base_path + 'test/{:02d}/info/info_{:05d}.txt'
+
+
+
 numTotal=   1195
 objId = 5
 
 
-trainScenesFile = open(trainScenes_file_path.format(objId), 'w')
-testScenesFile = open(testScenes_file_path.format(objId), 'w')
+def genTrainTestFiles():
+    trainScenesFile = open(trainScenes_file_path.format(objId), 'w')
+    testScenesFile = open(testScenes_file_path.format(objId), 'w')
 
-perms = np.random.permutation(numTotal)
-print perms
-numTrain = int(np.floor(numTotal*0.8))
-idx = perms[0:numTrain]
-idx=np.sort(idx)
-for i in idx:
-    trainScenesFile.write('%d\n'%(i+1))
-idx = perms[numTrain:]
-idx=np.sort(idx)
+    perms = np.random.permutation(numTotal)
+    print perms
+    numTrain = int(np.floor(numTotal*0.8))
+    idx = perms[0:numTrain]
+    idx=np.sort(idx)
+    for i in idx:
+        trainScenesFile.write('%d\n'%(i+1))
+    idx = perms[numTrain:]
+    idx=np.sort(idx)
 
-for i in idx:
-    testScenesFile.write('%d\n'%(i+1))
+    for i in idx:
+        testScenesFile.write('%d\n'%(i+1))
+
+
+def separateTrainTestImages():
+    trainScenesFile = open(trainScenes_file_path.format(objId), 'r')
+    testScenesFile = open(testScenes_file_path.format(objId), 'r')
+
+    # for line in trainScenesFile.readlines():
+    #     print line
+    #     # print 'cp '+ rgb_in_mpath.format(objId,int(line))+ base_path+'test/%02d/training/rgb/'%(objId)
+    #
+    #     os.system('cp '+ rgb_in_mpath.format(objId,int(line))+' '+ base_path+'test/%02d/training/rgb/'%(objId))
+    #     os.system('cp '+ depth_in_mpath.format(objId,int(line))+' '+ base_path+'test/%02d/training/depth/'%(objId))
+    #
+    #     os.system('cp '+ seg_in_path.format(objId,int(line))+' '+ base_path+'test/%02d/training/seg/'%(objId))
+    #
+    #     os.system('cp '+ labelled_mpath.format(objId,int(line))+' '+ base_path+'test/%02d/training/labelled/'%(objId))
+
+
+    for line in testScenesFile.readlines():
+        print line
+        # print 'cp '+ rgb_in_mpath.format(objId,int(line))+ base_path+'test/%02d/training/rgb/'%(objId)
+
+        # os.system('cp '+ rgb_in_mpath.format(objId,int(line))+' '+ base_path+'test/%02d/test/rgb/'%(objId))
+        # os.system('cp '+ depth_in_mpath.format(objId,int(line))+' '+ base_path+'test/%02d/test/depth/'%(objId))
+        #
+        # # os.system('cp '+ seg_in_path.format(objId,int(line))+' '+ base_path+'test/%02d/training/seg/'%(objId))
+        # os.system('cp '+ labelled_output_mpath.format(objId,int(line))+' '+ base_path+'test/%02d/test/labelled/'%(objId))
+        os.system('cp '+ info_in_path.format(objId,int(line))+' '+ base_path+'test/%02d/test/info/'%(objId))
+
+
+separateTrainTestImages()
